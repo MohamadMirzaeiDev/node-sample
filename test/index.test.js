541265dea4a1038e3980220b810a04e858f091cd
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2024-02-10 22:07:58 +0330

    clean empty space

--- test/index.test.js
+++ test/index.test.js
@@ -1,13 +1,11 @@
 const app = require("../src")
 const request = require('supertest')
 
+
 describe('test app', () => { 
-   test('It should response the GET method' , done =>{
-    request(app)
+   test('It should response the GET method' , () =>{
+    return request(app)
     .get('/')
-    .then(res=>{
-      expect(res.statusCode).toBe(200)
-      done()  
-    })
+    .expect(200)
    })
 })
