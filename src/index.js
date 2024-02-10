afaedb77d165199b341ee0f2ee6509bea5f0cc3f
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2024-02-10 17:18:53 +0330

    make src (main directory)

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express()
+const port = process.env.PORT || 3001 ;
+
+
+
+app.get("/" , (req,res)=>{
+	res.status(200).send("hello world")
+});
+
+
+app.listen(port , ()=>{
+	console.log("app runned in port " , port)
+})
+
+
+module.exports = app ; 
